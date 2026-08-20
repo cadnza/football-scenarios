545a4e520b4a4398246272929c47ee10c20d68cc
Jon Dayley <jonathandayley@gmail.com>
2026-08-20 13:11:05 -0600

    Move to library

---
+++ libs/config.py
@@ -0,0 +1,84 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import cast
+
+# Orient
+_here = Path(__file__).parent.parent
+
+# Load schema
+with (_here / "schemas" / "plan.json").open() as f_schema:
+    _schema = json.load(f_schema)
+
+    # Read tactics
+    tactics = cast("list[str]", _schema["properties"]["tactic"]["enum"])
+
+    # Read levels
+    levels = cast("list[str]", _schema["properties"]["level"]["enum"])
+
+
+@dataclass(frozen=True, kw_only=True)
+class Config:
+    """A plan/scenario configuration."""
+
+    idx: int
+    """This config's index."""
+
+    tactic: str
+    """This config's tactic."""
+
+    level: str
+    """This config's level."""
+
+    sequence_number: int
+    """This config's sequence number."""
+
+    def increment(self) -> "Config":
+        """Labels the next file."""
+        # Increment everything by one rotation
+        index = self.idx + 1
+        if self.level == levels[-1]:
+            level = levels[0]
+            i_tactic_next = tactics.index(self.tactic) + 1
+            tactic = (
+                tactics[i_tactic_next] if i_tactic_next < len(tactics) else tactics[0]
+            )
+            sequence_number = (
+                self.sequence_number + 1
+                if tactic == tactics[0] and level == levels[0]
+                else self.sequence_number
+            )
+        else:
+            level = levels[levels.index(self.level) + 1]
+            tactic = self.tactic
+            sequence_number = self.sequence_number
+
+        # Return
+        return Config(
+            idx=index,
+            tactic=tactic,
+            level=level,
+            sequence_number=sequence_number,
+        )
+
+    @property
+    def title(self) -> str:
+        """This config's title."""  # noqa: D404
+        return f"{self.idx}-{self.tactic}-{self.level}-{self.sequence_number}"
+
+    @property
+    def filename(self) -> str:
+        """The basename of the file represented by this config."""
+        return f"{self.title}.yaml"
+
+    @property
+    def filepath(self) -> Path:
+        """The path of the file represented by this config."""
+        dir_plans = _here / "plans"
+        dir_plans.mkdir(parents=True, exist_ok=True)
+        return dir_plans / self.filename
+
+    def write_stub(self) -> None:
+        """Write this file's stub."""
+        with self.filepath.open("w", encoding="utf-8") as f:
+            f.writelines([f"tactic: {self.tactic}\n", f"level: {self.level}\n"])
